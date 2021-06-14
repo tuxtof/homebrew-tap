1b694f07cb529ab66a26fa07fbd23eeff667e1e2
Christophe Jauffret <reg-github@geo6.net>
2021-06-14 16:36:01 +0200

    fix2

--- Formula/kubectl-karbon.rb
+++ Formula/kubectl-karbon.rb
@@ -1,4 +1,4 @@
-class Kubectl_karbon < Formula
+class KubectlKarbon < Formula
     desc "kubectl karbon plugin - Easy connect to Nutanix karbon clusters"
     homepage "https://github.com/nutanix/kubectl-karbon"
     url "https://github.com/nutanix/kubectl-karbon/releases/download/v0.2.0/kubectl-karbon_v0.2.0_darwin_amd64.tar.gz"
