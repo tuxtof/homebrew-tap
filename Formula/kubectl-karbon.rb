7c1f2f4e8a3ccd219c06289e769493d232beb2ce
Christophe Jauffret <reg-github@geo6.net>
2021-06-14 17:03:01 +0200

    update formula

--- Formula/kubectl-karbon.rb
+++ Formula/kubectl-karbon.rb
@@ -3,12 +3,15 @@ class KubectlKarbon < Formula
     homepage "https://github.com/nutanix/kubectl-karbon"
     url "https://github.com/nutanix/kubectl-karbon/releases/download/v0.2.0/kubectl-karbon_v0.2.0_darwin_amd64.tar.gz"
     sha256 "866b4398547173433163e33912c2fcae91415b6fef2c434a498bceb2afa69fcf"
+    license "Apache-2.0"
     
+    depends_on "kubectl"
+
     def install
       bin.install "kubectl-karbon"
     end
   
     test do
-      system "false"
+      system "kubectl" "karbon" "help"
     end
   end
